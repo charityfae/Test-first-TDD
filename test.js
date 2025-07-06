ddafe7455c61c06a008841b3113535902fb9968a
Ojera Charity Faith <faeojera@gmail.com>
2025-07-06 11:37:44 +0300

    code refactored

--- test.js
+++ test.js
@@ -1,4 +1,17 @@
-assertEqual(multiply(1,1), 1);
+//first code cycle one
+/*assertEqual(multiply(1,1), 1);
 function multiply(a, b) {
-  return 1;
+    return 1;
 }
+*/
+
+
+const assert = require('assert');
+
+function multiply(a, b) {
+  return a * b;
+}
+
+assert.strictEqual(multiply(1, 1), 1);
+
+
