bd71f26b2ee865304d7450404e9a6d93e9a59ecb
Ojera Charity Faith <faeojera@gmail.com>
2025-07-10 15:56:17 +0300

    added other tests

--- taxcalculator.js
+++ taxcalculator.js
@@ -1,5 +1,13 @@
 function calculateTax(income) {
-  return 0; // First passing implementation for test
+  if (income <= 12000) {
+    return 0;
+  } else if (income <= 36000) {
+    return (income - 12000) * 0.2;
+  } else {
+    const baseTax = (36000 - 12000) * 0.2; // 4800
+    const upperTax = (income - 36000) * 0.4; // 40% of the amount above 36000
+    return baseTax + upperTax;
+  }
 }
 
 module.exports = { calculateTax };
